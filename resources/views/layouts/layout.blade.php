a254d0d381a0795be9a95072b48528bf0aaa771a
Fauzan Arrasyid <fauzan.sullivan@gmail.com>
2018-10-10 19:16:09 +0700

    Update List petugas

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -24,6 +24,7 @@
     <link href="{{ asset('dashboard_asset/plugins/c3-master/c3.min.css') }}" rel="stylesheet">
     <!-- Vector CSS -->
     <link href="{{ asset('dashboard_asset/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet" />
+    <link href="{{ asset('dashboard_asset/plugins/datatables/media/css/dataTables.bootstrap4.css') }}" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="{{ asset('dashboard_asset/css/style.css') }}" rel="stylesheet">
     <!-- You can change the theme colors from here -->
@@ -248,6 +249,63 @@
     <script src="{{ asset('dashboard_asset/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
     <script src="{{ asset('dashboard_asset/plugins/vectormap/jquery-jvectormap-us-aea-en.js') }}"></script>
     <script src="{{ asset('dashboard_asset/js/dashboard2.js') }}"></script>
+    <script src="{{ asset('dashboard_asset/plugins/datatables/datatables.min.js') }}"></script>
+    <!-- start - This is for export functionality only -->
+    <script src="{{ asset('dashboard_asset/plugins/cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('dashboard_asset/plugins/cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js') }}"></script>
+    <script src="{{ asset('dashboard_asset/plugins/cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js') }}"></script>
+    <script src="{{ asset('dashboard_asset/plugins/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('dashboard_asset/plugins/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('dashboard_asset/plugins/cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('dashboard_asset/plugins/cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js') }}"></script>
+    <!-- end - This is for export functionality only -->
+    <script>
+        $(function () {
+            $('#myTable').DataTable();
+            $(function () {
+                var table = $('#example').DataTable({
+                    "columnDefs": [{
+                        "visible": false,
+                        "targets": 2
+                    }],
+                    "order": [
+                        [2, 'asc']
+                    ],
+                    "displayLength": 25,
+                    "drawCallback": function (settings) {
+                        var api = this.api();
+                        var rows = api.rows({
+                            page: 'current'
+                        }).nodes();
+                        var last = null;
+                        api.column(2, {
+                            page: 'current'
+                        }).data().each(function (group, i) {
+                            if (last !== group) {
+                                $(rows).eq(i).before('<tr class="group"><td colspan="5">' + group + '</td></tr>');
+                                last = group;
+                            }
+                        });
+                    }
+                });
+                // Order by the grouping
+                $('#example tbody').on('click', 'tr.group', function () {
+                    var currentOrder = table.order()[0];
+                    if (currentOrder[0] === 2 && currentOrder[1] === 'asc') {
+                        table.order([2, 'desc']).draw();
+                    } else {
+                        table.order([2, 'asc']).draw();
+                    }
+                });
+            });
+        });
+        $('#example23').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
+        });
+    </script>
     <!-- ============================================================== -->
     <!-- Style switcher -->
     <!-- ============================================================== -->

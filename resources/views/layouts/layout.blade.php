1603542d48251456c9ab3d50e3e32999e7054446
cecepshiro <saefudincecep@gmail.com>
2018-10-17 14:50:55 +0700

    fix menu

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -162,11 +162,24 @@
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
                         <li class="nav-small-cap">Dashboard</li>
-                        <li class="active">
-                            <a class="waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard </span></a>
-                           
+                        <li class="">
+                            <a class="waves-effect waves-dark" href="{{ url('/')}}" aria-expanded="false"><i class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard </span></a>
                         </li>
-                       
+                        <li class="">
+                        <a class="waves-effect waves-dark" href="{{ route('barang.index')}}" aria-expanded="false"><i class="mdi mdi-water"></i><span class="hide-menu">Barang </span></a>
+                        </li>
+                        <li class="">
+                            <a class="waves-effect waves-dark" href="{{ route('petugas.index')}}" aria-expanded="false"><i class="mdi mdi-account"></i><span class="hide-menu">Petugas </span></a>
+                        </li>
+                        <li class="">
+                            <a class="waves-effect waves-dark" href="{{ route('perusahaan.index')}}" aria-expanded="false"><i class="mdi mdi-bank"></i><span class="hide-menu">Perusahaan </span></a>
+                        </li>
+                        <li class="">
+                            <a class="waves-effect waves-dark" href="{{ route('lab.index')}}" aria-expanded="false"><i class="mdi mdi-thermometer"></i><span class="hide-menu">Laboratorium </span></a>
+                        </li>
+                        <li class="">
+                            <a class="waves-effect waves-dark" href="{{ route('pengujian.index')}}" aria-expanded="false"><i class="mdi mdi-thermometer-lines"></i><span class="hide-menu">Pengujian </span></a>
+                        </li>
                     </ul>
                 </nav>
                 <!-- End Sidebar navigation -->

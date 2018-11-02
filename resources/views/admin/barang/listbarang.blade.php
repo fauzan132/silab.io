ca4bd594203f625b52e3b5b64832fe52bb6ffb93
Fauzan Arrasyid <fauzan.sullivan@gmail.com>
2018-11-02 23:52:40 +0700

    Update Btn Listbarang

--- resources/views/admin/barang/listbarang.blade.php
+++ resources/views/admin/barang/listbarang.blade.php
@@ -26,6 +26,7 @@
                             <div class="card-body">
                                 <h4 class="card-title">Data Barang</h4>
                                 <div class="table-responsive m-t-40">
+                                <button type="button" class="btn btn-sm waves-effect waves-light btn-success"><i class="ti-plus"></i> Tambah Data</button>
                                     <table id="myTable" class="table table-bordered table-striped">
                                         <thead>
                                             <tr>
